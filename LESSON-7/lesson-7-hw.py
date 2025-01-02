db2add3f37df5f91142ad7b3c2eef6283dde54cd
Kiran <ftd10622112@dseu.ac.in>
2025-01-02 12:09:00 +0530

    lesson-7

---
+++ LESSON-7/lesson-7-hw.py
@@ -0,0 +1,23 @@
+# In your class, there are five groups that participated and got prizes in 
+# different competitions held in various schools. 
+# Your class teacher asks your help to record all the details. 
+# Write a program and use a tuple data structure to store each record. 
+# The program should ask the user to enter details like groupname, Sizeofthegroup, 
+# dateofthecompetition, venue, typeofthemedal. Store it as a tuple.
+
+# This should be done for all the five groups.
+
+from sqlite3 import Date
+
+
+records = {}
+
+for i in range(5):
+    groupname = input("Enter your group name")
+    group_size = int(input("Enter the size of the group"))
+    date = input("enter date as yy:mm:dd")
+    venue = input("enter the venue")
+    medal = input("enter your medal type")
+    records[groupname] = group_size, date, venue, medal
+
+print(records)
